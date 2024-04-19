b5410254337b5282e51c1217f4b35757bf40d513
Shradha Samudrala <dundigallas@mail.sacredheart.edu>
2024-04-20 02:37:38 +0530

    first commit

---
+++ Basic-Operations CW1.R
@@ -0,0 +1,29 @@
+"Hello, world"
+V1 <-10
+V2 <- sqrt(V1)
+
+Name<-"Sanjana"
+College<- "SHU"
+Details<- paste (Name, College)
+
+x<-4L
+typeof(x)
+
+y<-8.5
+typeof(y)
+
+a<-5
+b<-9
+c<-a+b
+
+result <- 9<10
+result
+typeof (result)
+
+result2 <- !(10>5)
+result2
+
+result | result2
+result & result2
+isTRUE (result)
+#small change
